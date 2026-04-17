5f1b080bf49d28ed7db84f1524e5abfe1619d954
Hiep Nguyen <nguyenhuuhiep1990@gmail.com>
2026-04-17 10:53:36 +0700

    add wso2

---
+++ infrastructure/gateway/wso2am-4.6.0/repository/deployment/server/webapps/devportal/site/public/dist/2608.1152568676b04e37c855.bundle.js
@@ -0,0 +1,2 @@
+(()=>{"use strict";setInterval((()=>{self.postMessage("")}),1e3)})();
+//# sourceMappingURL=2608.1152568676b04e37c855.bundle.js.map
---
+++ infrastructure/gateway/wso2am-4.6.0/repository/deployment/server/webapps/publisher/site/public/dist/2608.55c82628f032bd63f1c0.bundle.js
@@ -0,0 +1,2 @@
+(()=>{"use strict";setInterval(function(){self.postMessage("")},1e3)})();
+//# sourceMappingURL=2608.55c82628f032bd63f1c0.bundle.js.map
